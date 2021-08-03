b6a9a583d67b4c6797b2ab2cfc7c7ea2836ff4fc
Muzdalifa <a.muzdalifa27@gmail.com>
2021-08-03 14:22:17 +0200

    Create functions to get the keypresses to move your player, up, down, left and right

--- myjavascript.js
+++ myjavascript.js
@@ -1,60 +1,89 @@
-function addBoardCell(){
-  for(let i = 0; i<tileMap01.mapGrid.length;i ++)
+function drawBoard(){
+
+  //1. clear board
+  document.getElementById("game-board").innerHTML =""
+
+  //2. drow board
+  for(let y = 0; y<tileMap01.mapGrid.length;y ++)
   {
-    //console.log(tileMap01.mapGrid[i]);
-    const row = tileMap01.mapGrid[i]
+    //console.log(tileMap01.mapGrid[y]);
+    const row = tileMap01.mapGrid[y]
     
-    for(let j =0; j<row.length;j++)
+    for(let x =0; x<row.length;x++)
     {
-      //console.log(row[j]);
+      //console.log(row[x]);
       let div = document.createElement("div");
       //every cells has it's unique id
-      div.id = `${i},${j}`;
+      div.id = `${x},${y}`;
       //add list of classes for each cell
       div.classList.add("board-cell");
 
       //add new class for styling cell
-      if(row[j][0] !==" "){
-        div.classList.add( row[j][0].toLowerCase());
+      if(row[x][0] !==" "){
+        div.classList.add( row[x][0].toLowerCase());
       }
       //add content of each cell
-      div.innerHTML = row[j][0]; // zero denotes content of the row array 
+      div.innerHTML = row[x][0]; // zero denotes content of the row array 
       //append child to the node
       document.getElementById("game-board").appendChild(div);
     }    
   }
 }
 
-addBoardCell();
+drawBoard();
 
 document.addEventListener('keydown', onKeyPressListner)
 
 function onKeyPressListner(event){
-console.log(event);
-event.preventDefault();
+  //console.log(event);
+  event.preventDefault();
 
-switch(event.key){
-  case "ArrowRight":
-  {
-    break;
+  //movePlayer()
+  playerDiv = document.getElementsByClassName("p")[0];
+
+  console.log(playerDiv.id)
+
+  const [x,y] = playerDiv.id.split(",").map(Number);
+  // console.log(i);
+  // console.log(j); 
+
+  switch(event.key){
+    case "ArrowUp":
+    {
+      const [destX, destY] = [x, y-1]
+      movePlayer(x, y, destX, destY);
+      break;
+    }
+    case "ArrowDown":
+    {
+      const [destX, destY] = [x, y+1]
+      movePlayer(x, y, destX, destY);
+      break;
+    }
+    case "ArrowLeft":
+    {
+      const [destX, destY] = [x-1, y];
+      movePlayer(x, y, destX, destY);
+      break;
+    }
+    case "ArrowRight":
+    {
+      const [destX, destY] = [x+1, y]
+      movePlayer(x, y, destX, destY);      
+      break;
+    }
   }
-  case "ArrowRight":
-  {
-    break;
-  }
-  case "ArrowRight":
-  {
-    break;
-  }
-  case "ArrowRight":
-  {
-    break;
-  }
-}
  
-
 }
 
-function movePlayer(){
+function movePlayer(x, y, destX, destY)
+{
+  //add player to the new position
+  tileMap01.mapGrid[destY][destX][0] = tileMap01.mapGrid[y][x][0];
 
+  // assign empty array player to the current position
+  tileMap01.mapGrid[y][x][0] = " "; 
+
+  //draw new board with new player position
+  drawBoard();  
 }
